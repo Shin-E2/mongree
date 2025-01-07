06637357b6ad8b0dcb73e0150b4db0c06f4c7cff
Shin-E2 <sinhyejee322@gmail.com>
2025-01-07 20:25:39 +0900

    [Feat/main] Next.js 설정 개선 - 클라이언트 측 Webpack 캐싱 비활성화 및 특정 원격 이미지 도메인 허용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Webpack 캐시 설정 (클라이언트에서 캐시 비활성화)
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = false;
+    }
+    return config;
+  },
+
+  // 이미지 도메인 허용
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "sesac.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // Google 프로필 이미지
+      },
+      {
+        protocol: "https",
+        hostname: "k.kakaocdn.net", // Kakao 프로필 이미지
+      },
+      {
+        protocol: "https",
+        hostname: "ssl.pstatic.net", // Naver 프로필 이미지
+      },
+    ],
+  },
 };
 
 export default nextConfig;

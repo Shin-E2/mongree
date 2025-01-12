1bbcb95683d6675eefb9a9c6cd46a22fb069c353
Shin-E2 <sinhyejee322@gmail.com>
2025-01-12 14:59:44 +0900

    [Refactor/main] InputBase 컴포넌트 개선: react-hook-form 통합, 에러 처리 및 스타일 업데이트

--- src/commons/components/input/index.tsx
+++ src/commons/components/input/index.tsx
@@ -1,23 +1,42 @@
+import type { FieldValues } from "react-hook-form";
 import styles from "./styles.module.css";
 import type { IInputBaseProps, IInputStandardSFullProps } from "./types";
 
 // input 컴포넌트
-export default function InputBase({
+export default function InputBase<T extends FieldValues>({
   cssprop,
   name,
   placeholder,
-  type,
-}: IInputBaseProps) {
+  type = "text",
+  errors,
+  required,
+  register,
+  onChange,
+  id,
+}: IInputBaseProps<T>) {
   return (
-    <input
-      className={`${cssprop} ${styles.common}`}
-      name={name}
-      placeholder={placeholder}
-      type={type}
-    />
+    <div className={styles.div}>
+      <input
+        className={type === "file" ? "hidden" : `${cssprop} ${styles.common}`}
+        {...(register ? register(name) : { name })}
+        placeholder={placeholder}
+        type={type}
+        required={required}
+        onChange={onChange}
+        id={id}
+      />
+      {/* {errors?.map((error, i) => (
+        <span key={i} className={styles.error}>
+          {error}
+        </span>
+      ))} */}
+      <span className={styles.error}>{errors}</span>
+    </div>
   );
 }
 
-export const InputStandardSFull = ({ ...rest }: IInputStandardSFullProps) => {
+export const InputStandardSFull = <T extends FieldValues>({
+  ...rest
+}: IInputStandardSFullProps<T>) => {
   return <InputBase {...rest} cssprop={styles.standard__s__full} />;
 };
--- src/commons/components/input/types.ts
+++ src/commons/components/input/types.ts
@@ -1,9 +1,18 @@
-export interface IInputBaseProps {
+import type { RefObject } from "react";
+import type { FieldValues, Path, UseFormRegister } from "react-hook-form";
+
+export interface IInputBaseProps<T extends FieldValues> {
   cssprop: string;
-  name: string;
-  placeholder: string;
-  type?: string;
+  name: Path<T>; // react-hook-form의 path타입
+  placeholder?: string;
+  type?: HTMLInputElement["type"];
+  errors?: string;
+  register?: UseFormRegister<T>; // register(react-hook-form)
+  required?: boolean;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  id?: string;
+  // ref?: RefObject<HTMLInputElement | null>;
 }
 
-export interface IInputStandardSFullProps
-  extends Omit<IInputBaseProps, "cssprop"> {}
+export interface IInputStandardSFullProps<T extends FieldValues>
+  extends Omit<IInputBaseProps<T>, "cssprop"> {}

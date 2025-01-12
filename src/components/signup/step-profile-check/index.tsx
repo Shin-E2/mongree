c078da5922b93ed932ba179feafa20064f0a72bf
Shin-E2 <sinhyejee322@gmail.com>
2025-01-12 15:04:11 +0900

    [Refactor/main] SignupStepProfileCheck: react-hook-form 폼 상태 관리 및 오류 처리 개선

--- src/components/signup/step-profile-check/index.tsx
+++ src/components/signup/step-profile-check/index.tsx
@@ -1,6 +1,13 @@
 import { ImagePreviewByProfile } from "@/commons/components/image-preview";
+import { useFormContext } from "react-hook-form";
+import type { SignupFormType } from "../step-basic-info/form.schema";
 
 export default function SignupStepProfileCheck() {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext<SignupFormType>();
+
   return (
     <div className="flex flex-col items-center space-y-6">
       <h2 className="text-xl font-semibold">프로필 설정</h2>

e8f273c4705bd98be9589aeaca36e599b2ca4759
Shin-E2 <sinhyejee322@gmail.com>
2025-01-12 14:54:46 +0900

    [Refactor/main] "@typescript-eslint/no-empty-object-type" 규칙 비활성화

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin"; // 플러그인 import
+import pluginReact from "eslint-plugin-react";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], // 린트 대상 파일
+    languageOptions: {
+      globals: globals.browser,
+      parser: "@typescript-eslint/parser", // TypeScript 파서 설정
+    },
+    plugins: {
+      "@typescript-eslint": tseslint, // 플러그인 등록
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules, // JavaScript 추천 규칙 포함
+      ...tseslint.configs.recommended.rules, // TypeScript 추천 규칙 포함
+      ...pluginReact.configs.flat.recommended.rules, // React 추천 규칙 포함
+      "@typescript-eslint/no-empty-object-type": "off", // 해당 규칙 비활성화
+    },
+  },
 ];
-
-export default eslintConfig;

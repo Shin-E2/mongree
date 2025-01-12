a7e75151fbb301300433c5e304df74de91340758
Shin-E2 <sinhyejee322@gmail.com>
2025-01-12 15:06:14 +0900

    [Refactor/mmain] SignupFormSteps컴포넌트 생성 및 코드 분리

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { CardStandardFullFull } from "@/commons/components/card";
-import SignupSectionButton from "@/components/signup/section-button";
 import SignupStepProgressSection from "@/components/signup/step-progress-section";
 import { SIGNUP_STEPS } from "@/components/signup/step-progress-section/constants";
 import styles from "./styles.module.css";
 
 import { useState } from "react";
+import SignupFormSteps from "@/components/signup/form-steps";
 
 export default function SignupPage() {
   // 현재 회원가입 단계(0: 기본정보, 1: 프로필설정)
@@ -16,20 +16,6 @@ export default function SignupPage() {
   const isLastStep = currentStep === SIGNUP_STEPS.length - 1;
   // currentStep이 0일 때 50%, 1일 때 100%
   const progress = ((currentStep + 1) / SIGNUP_STEPS.length) * 100;
-
-  // 다음
-  const handleNext = () => {
-    // 다음 단계로 이동
-    setCurrentStep((prev) => prev + 1);
-  };
-
-  // 이전
-  const handlePrev = () => {
-    setCurrentStep((prev) => prev - 1);
-  };
-
-  // 현재 단계의 컴포넌트
-  const SignupStepComponent = currentStepData.Component;
 
   return (
     <section className={styles.section}>
@@ -42,18 +28,12 @@ export default function SignupPage() {
 
         {/* 폼 영역 */}
         <CardStandardFullFull>
-          <form className="space-y-6">
-            {/* 현재 단계의 컴포넌트 렌더링 */}
-            <SignupStepComponent />
-
-            {/* 이전/다음 버튼 */}
-            <SignupSectionButton
-              currentStep={currentStep}
-              handleNext={handleNext}
-              isLastStep={isLastStep}
-              handlePrev={handlePrev}
-            />
-          </form>
+          <SignupFormSteps
+          currentStepData={currentStepData}
+            setCurrentStep={setCurrentStep}
+            currentStep={currentStep}
+            isLastStep={isLastStep}
+          />
         </CardStandardFullFull>
       </div>
     </section>

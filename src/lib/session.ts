bd9bfe96f7805fc193b12caab167dfc4d2ac78e8
Shin-E2 <sinhyejee322@gmail.com>
2025-01-20 20:50:56 +0900

    [Feat/main] 세션 관리: iron-session 통합으로 세션 관리 추가

---
+++ src/lib/session.ts
@@ -0,0 +1,15 @@
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+
+interface SessionContent {
+  id?: string; //로그인한 사용자만 가지고 있음
+}
+
+export async function getSession() {
+  // console.log("쿠키:", cookies());
+
+  return getIronSession<SessionContent>(await cookies(), {
+    cookieName: "mongree",
+    password: process.env.COOKIE_PASSWORD!,
+  });
+}

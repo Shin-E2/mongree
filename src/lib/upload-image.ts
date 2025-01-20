29637e99d242ab66b281414c9f61bd0e0865a606
Shin-E2 <sinhyejee322@gmail.com>
2025-01-20 20:51:06 +0900

    [Feat/main] S3 이미지 업로드 기능 추가: AWS S3에 파일 업로드를 위한 uploadImageToS3 함수 구현

---
+++ src/lib/upload-image.ts
@@ -0,0 +1,35 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({
+  region: process.env.AWS_REGIN,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+export async function uploadImageToS3(file: File) {
+  const fileName = `${Date.now()}-${file.name}`;
+  const bucketName = process.env.AMPLIFY_BUCKET!;
+  const region = process.env.AWS_REGION!;
+
+  //File 객체를 ArraryBuffer로 변환
+  const photoData = await file.arrayBuffer();
+
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: fileName,
+    Body: new Uint8Array(photoData), // Uint8Array로 변환하여 전달
+    ContentType: file.type, // 파일 MIME 타입 설정
+  });
+
+  try {
+    await s3Client.send(command);
+
+    //s3 객체 url 반환
+    return `https://${bucketName}.s3.${region}.amazonaws.com/${fileName}`;
+  } catch (error) {
+    console.error("S3 업로드 실패:", error);
+    throw new Error("파일 업로드 중 문제가 발생했습니다.");
+  }
+}

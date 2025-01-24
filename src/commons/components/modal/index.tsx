993e38fbc63b216b8edfc4e5c13a44658b1753ea
Shin-E2 <sinhyejee322@gmail.com>
2025-01-24 11:32:29 +0900

    [Feat/main] 모달 컴포넌트 기능 확장: 제목, 설명, 버튼 옵션 추가

--- src/commons/components/modal/index.tsx
+++ src/commons/components/modal/index.tsx
@@ -2,11 +2,17 @@ import ModalPortal from "@/commons/portal/modal-portal";
 import styles from "./styles.module.css";
 import type { IModalBaseProps, IModalCommonProps } from "./types";
 import { ButtonIconDeleteNoBackground } from "../button-icon";
+import { ButtonTextStandardSFull } from "../button-text";
 
 export default function ModalBase({
   cssprop,
   children,
   onClose,
+  isAddress,
+  title,
+  discription,
+  okButton,
+  cancelButton,
 }: IModalBaseProps) {
   return (
     <ModalPortal>
@@ -14,8 +20,41 @@ export default function ModalBase({
       <div className={styles.container}>
         {/* 모달안의 영역 */}
         <div className={` ${cssprop} ${styles.common}`}>
-          <ButtonIconDeleteNoBackground onClick={onClose} />
-          {children}
+          {/* 주소일때 */}
+          {isAddress && (
+            <>
+              <ButtonIconDeleteNoBackground onClick={onClose} />
+              {children}
+            </>
+          )}
+          {(title ?? discription) && (
+            <>
+              {title && <span className="text-2xl font-bold">{title}</span>}
+              {discription && (
+                <p className="text-gray-500 text-sm whitespace-pre-line text-center pt-2">
+                  {discription}
+                </p>
+              )}
+            </>
+          )}
+          {(okButton ?? cancelButton) && (
+            <div className={styles.common__buttons}>
+              {cancelButton?.text && (
+                <ButtonTextStandardSFull
+                  title={cancelButton?.text}
+                  type="button"
+                  onClick={cancelButton.onClick}
+                />
+              )}
+              {okButton?.text && (
+                <ButtonTextStandardSFull
+                  title={okButton?.text}
+                  type="button"
+                  onClick={okButton.onClick}
+                />
+              )}
+            </div>
+          )}
         </div>
       </div>
     </ModalPortal>
@@ -24,4 +63,8 @@ export default function ModalBase({
 
 export const ModalStandardFullFUll = ({ ...rest }: IModalCommonProps) => {
   return <ModalBase cssprop={styles.standard__full__full} {...rest} />;
+};
+
+export const ModalStandardFitFit = ({ ...rest }: IModalCommonProps) => {
+  return <ModalBase cssprop={styles.standard__fit__fit} {...rest} />;
 };
--- src/commons/components/modal/types.ts
+++ src/commons/components/modal/types.ts
@@ -1,7 +1,18 @@
 export interface IModalBaseProps {
   cssprop: string;
-  children: React.ReactNode;
-  onClose: () => void;
+  children?: React.ReactNode;
+  onClose?: () => void;
+  isAddress?: boolean;
+  title?: string;
+  discription?: string;
+  okButton?: {
+    text: string;
+    onClick: () => void | Promise<void>;
+  };
+  cancelButton?: {
+    text: string;
+    onClick?: () => void | Promise<void>;
+  };
 }
 
 export interface IModalCommonProps extends Omit<IModalBaseProps, "cssprop"> {}

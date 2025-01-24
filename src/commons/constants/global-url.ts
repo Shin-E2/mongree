72340b717ba427b82fcb124c592d18ee2a48d25a
Shin-E2 <sinhyejee322@gmail.com>
2025-01-24 11:30:30 +0900

    [Feat/main] 글로벌 URL 상수 추가: 애플리케이션 라우팅 경로 정의

---
+++ src/commons/constants/global-url.ts
@@ -0,0 +1,24 @@
+export const URL = () => ({
+  // 로그인
+  LOGIN: `/login`,
+
+  // 회원가입
+  SIGNUP: `/signup`,
+
+  // 메인 페이지
+  HOME: `/home`,
+
+  // 일기 작성
+  DIARY: `/diary`,
+
+  // 마이 페이지
+  PROFILE: `/profile`,
+
+  CALENDAR: `/calendar`,
+
+  STATISTICS: `/statistics`,
+
+  COMMUNITY: `/community`,
+  
+  COUNSELORS: `/counselors`,
+});

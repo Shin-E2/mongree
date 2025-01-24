7ee362b775d69a911a5cbb8de132fbf6bb3f1454
Shin-E2 <sinhyejee322@gmail.com>
2025-01-24 11:27:19 +0900

    [Feat/main] 로그인 기능 추가: 성공적인 로그인 시 사용자 ID를 세션 객체에 저장하고 세션 데이터를 쿠키에 저장

---
+++ src/lib/login.ts
@@ -0,0 +1,13 @@
+import { getSession } from "./session";
+
+/* 
+- 사용자가 로그인에 성공하면 id를 섹션 객체에 저장
+- session.save()는 업데이트된 세션 데이터를 쿠키에 저장
+*/
+
+// user정보가 넘어옴
+export const Login = async (id: string) => {
+  const session = await getSession();
+  session.id = id; // 사용자 id를 세션에 저장
+  await session.save(); // 세션을 저장
+};

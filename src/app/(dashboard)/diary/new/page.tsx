3afeb41679e014a3ad42474cf2fae060d576d024
Shin-E2 <sinhyejee322@gmail.com>
2025-02-06 23:41:34 +0900

    [Refactor/main] 일기 작성 폼 스키마 개선

--- src/app/(dashboard)/diary/new/page.tsx
+++ src/app/(dashboard)/diary/new/page.tsx
@@ -6,7 +6,10 @@ import DiaryNewSectionButton from "@/components/home/(dashboard)/diary/new/secti
 import { CardStandardFullFull } from "@/commons/components/card";
 import DiaryNewHeader from "@/components/home/(dashboard)/diary/new/header";
 import { FormDiaryNew } from "@/commons/components/form";
-import { DiaryNewFormSchema } from "@/components/home/(dashboard)/diary/new/form.schema";
+import {
+  DiaryNewFormSchema,
+  type DiaryNewFormType,
+} from "@/components/home/(dashboard)/diary/new/form.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function DiaryNewPage() {
@@ -23,7 +26,7 @@ export default function DiaryNewPage() {
     DiaryNewStepComponent,
   } = useDiaryNewPage();
 
-  const onSubmit = (data: FormData) => {
+  const onSubmit = (data: DiaryNewFormType) => {
     console.log("내가 입력한게 멀까여?", data);
   };
 
@@ -33,11 +36,7 @@ export default function DiaryNewPage() {
       resolver={zodResolver(DiaryNewFormSchema)}
     >
       {/* Header */}
-      <DiaryNewHeader
-        setIsPublic={setIsPublic}
-        isPublic={isPublic}
-        selectedEmotions={selectedEmotions}
-      />
+      <DiaryNewHeader />
 
       {/* Main Content */}
       <div className="pt-24 pb-12 px-6 max-w-5xl mx-auto">
@@ -46,10 +45,7 @@ export default function DiaryNewPage() {
 
         {/* 현재 컴포넌트 */}
         <CardStandardFullFull>
-          <DiaryNewStepComponent
-            selectedEmotions={selectedEmotions}
-            setSelectedEmotions={setSelectedEmotions}
-          />
+          <DiaryNewStepComponent selectedEmotions={selectedEmotions} />
         </CardStandardFullFull>
 
         {/* 버튼 */}

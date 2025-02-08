3811e6c3d0c9eb73dc1d83b83441a16188c276a2
Shin-E2 <sinhyejee322@gmail.com>
2025-02-08 18:46:10 +0900

    [Refactor/main] 사용자 조회 로직 개선: 세션 및 404 처리

--- src/lib/get-user.ts
+++ src/lib/get-user.ts
@@ -6,16 +6,20 @@ import { getSession } from "./session";
 
 export async function getUser() {
   const session = await getSession();
-  if (session.id) {
-    const user = await db.user.findUnique({
-      where: {
-        id: session.id,
-      },
-    });
-    if (user) return user;
-  } else {
-    console.log("쿠키가 없습니다. 로그인을 다시 해주세요.");
+
+  if (!session.id) {
+    notFound();
   }
-  //로그인 안한 사용자가 프로필 페이지로 들어오면 404 status페이지 보여줌
-  notFound();
+
+  const user = await db.user.findUnique({
+    where: {
+      id: session.id,
+    },
+  });
+
+  if (!user) {
+    notFound();
+  }
+
+  return user;
 }

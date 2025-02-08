5b96affdabd1641040444d5b534caf2306eb4a24
Shin-E2 <sinhyejee322@gmail.com>
2025-02-08 18:47:00 +0900

    [Refactor/main] 일기 작성 섹션 버튼 로직 최적화: 감정 선택 조건 추가

--- src/components/home/(dashboard)/diary/new/section-button/index.tsx
+++ src/components/home/(dashboard)/diary/new/section-button/index.tsx
@@ -2,13 +2,14 @@ import {
   ButtonIconNext,
   ButtonIconPrev,
 } from "@/commons/components/button-icon";
+import { useFormContext } from "react-hook-form";
+import type { DiaryNewFormType } from "../form.schema";
 
 export interface IDiaryNewSectionButton {
   currentStep: number;
   handleNext: () => void;
   handlePrev: () => void;
   isLastStep: boolean;
-  isNextButtonEnabled: boolean;
 }
 
 export default function DiaryNewSectionButton({
@@ -16,12 +17,15 @@ export default function DiaryNewSectionButton({
   handleNext,
   handlePrev,
   isLastStep,
-  isNextButtonEnabled,
 }: IDiaryNewSectionButton) {
+  const { watch } = useFormContext<DiaryNewFormType>();
+  const emotions = watch("emotions") || [];
+  console.log("emotions", emotions);
+
   return (
     <div className="flex justify-center mt-8 space-x-4">
       {currentStep > 0 && <ButtonIconPrev onClick={handlePrev} type="button" />}
-      {!isLastStep && isNextButtonEnabled && (
+      {!isLastStep && emotions.length > 0 && (
         <ButtonIconNext onClick={handleNext} type="button" />
       )}
     </div>

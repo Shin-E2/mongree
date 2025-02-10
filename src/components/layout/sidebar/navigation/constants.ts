90daa8eb9f51c3091917fc65ad722a2fd56b3cff
Shin-E2 <sinhyejee322@gmail.com>
2025-02-10 23:19:00 +0900

    [Feat/main] 모바일 네비게이션 상수 추가: 하단 메뉴 아이템 정의

--- src/components/layout/sidebar/navigation/constants.ts
+++ src/components/layout/sidebar/navigation/constants.ts
@@ -1,11 +1,29 @@
+import {
+  Home,
+  BarChart2,
+  BookHeart,
+  BookOpen,
+  Calendar,
+  User,
+} from "lucide-react";
 import { URL } from "@/commons/constants/global-url";
 import type { NavItem } from "./types";
-import { BarChart2, BookHeart, BookOpen, Calendar, User } from "lucide-react";
 
+// 데스크탑
 export const NAV_ITEMS: NavItem[] = [
   { icon: BookHeart, label: "나의 일기", path: URL().DIARY },
   { icon: Calendar, label: "감정 캘린더", path: URL().CALENDAR },
   { icon: BarChart2, label: "감정 통계", path: URL().STATISTICS },
   { icon: BookOpen, label: "공개 일기", path: URL().COMMUNITY },
   { icon: User, label: "상담사 찾기", path: URL().COUNSELORS },
+];
+
+// 모바일
+export const MOBILE_NAV_ITEMS: NavItem[] = [
+  { icon: BookHeart, label: "나의 일기", path: URL().DIARY },
+  { icon: Calendar, label: "감정 캘린더", path: URL().CALENDAR },
+  { icon: Home, label: "", path: URL().HOME },
+  { icon: BarChart2, label: "감정 통계", path: URL().STATISTICS },
+  { icon: BookOpen, label: "공개 일기", path: URL().COMMUNITY },
+  // { icon: User, label: "상담사 찾기", path: URL().COUNSELORS },
 ];

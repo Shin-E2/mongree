8c35842edea00bd561ba9b6c44edaac07cd1eba2
Shin-E2 <sinhyejee322@gmail.com>
2025-02-10 23:19:12 +0900

    [Feat/main] 모바일 하단 네비게이션 레이아웃 구현: 반응형 메뉴 추가

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,20 +1,28 @@
+import styles from "./styles.module.css";
 import SidebarNavigation from "./navigation";
 import SidebarUserProfileSection from "./user-profile-section";
 import { ButtonTextLogo } from "@/commons/components/button-text";
 import { URL } from "@/commons/constants/global-url";
-import styles from "./styles.module.css";
+import MobileNavigation from "./navigation/mobile-navigation";
 
 export default function SideBar() {
   return (
-    <aside className={styles.aside}>
-      <div className={styles.aside_div}>
-        {/* Logo */}
-        <ButtonTextLogo href={URL().HOME} title="mongree" />
-        {/* Navigation */}
-        <SidebarNavigation />
-        {/* User Profile */}
-        <SidebarUserProfileSection />
-      </div>
-    </aside>
+    <>
+      {/* 데스크탑 사이드바 */}
+      <aside className={styles.aside}>
+        <div className={styles.aside_div}>
+          <ButtonTextLogo href={URL().HOME} title="mongree" />
+          <SidebarNavigation />
+          <SidebarUserProfileSection />
+        </div>
+      </aside>
+
+      {/* 모바일 하단 네비게이션 */}
+      <nav className={styles.mobile_nav}>
+        <div className={styles.mobile_nav_container}>
+          <MobileNavigation />
+        </div>
+      </nav>
+    </>
   );
 }

e8bd7dee97c09372e96ffd717f815ec6833eeb79
Shin-E2 <sinhyejee322@gmail.com>
2025-02-11 23:59:23 +0900

    [Style/main] 홈 감정 통계 아이템 스타일 및 아이콘 업데이트: 감정 분포 시각화 개선

--- src/components/home/(sidebar)/emotion-stats/index.tsx
+++ src/components/home/(sidebar)/emotion-stats/index.tsx
@@ -4,11 +4,21 @@ export default function HomeEmotionStats() {
   return (
     <div className="bg-white rounded-xl shadow-sm p-6">
       <h3 className="text-lg font-semibold mb-4">이번 달 감정 분포</h3>
-      <div className="space-y-4">
+      <div className="flex flex-col gap-4">
         <HomeEmotionStatItem
-          emotion="/images/sad.png"
-          percentage={45}
-          color="yellow"
+          emotion="/image/emotions/sad.svg"
+          percentage={25}
+          color="blue"
+        />
+        <HomeEmotionStatItem
+          emotion="/image/emotions/angry.svg"
+          percentage={15}
+          color="blue"
+        />
+        <HomeEmotionStatItem
+          emotion="/image/emotions/joyful.svg"
+          percentage={60}
+          color="blue"
         />
         {/* 기타 등등의 감정 */}
       </div>

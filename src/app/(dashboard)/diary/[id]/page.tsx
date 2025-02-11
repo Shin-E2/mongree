d32d6c6e7587a5059a892307fb5f578c3758d3ae
Shin-E2 <sinhyejee322@gmail.com>
2025-02-12 00:01:00 +0900

    [Feat/main] 일기 상세 페이지에 로그인 사용자 정보 추가

--- src/app/(dashboard)/diary/[id]/page.tsx
+++ src/app/(dashboard)/diary/[id]/page.tsx
@@ -1,6 +1,7 @@
 import DiaryDetailContent from "@/components/home/(dashboard)/diary/detail";
 import { notFound } from "next/navigation";
 import { getDiaryDetail } from "./action";
+import { getUser } from "@/lib/get-user";
 
 interface Props {
   params: {
@@ -10,13 +11,17 @@ interface Props {
 
 export default async function DiaryDetailPage({ params }: Props) {
   try {
-    const diary = await getDiaryDetail(params.id);
+    const { id } = await params;
+    const [diary, loginUser] = await Promise.all([
+      getDiaryDetail(id),
+      getUser(),
+    ]);
 
     if (!diary) {
       return notFound();
     }
 
-    return <DiaryDetailContent diary={diary} />;
+    return <DiaryDetailContent diary={diary} loginUser={loginUser} />;
   } catch (error) {
     console.error("일기 상세 조회 중 오류:", error);
     return notFound();

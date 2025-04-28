91c4befc72c0bc8681fb9746bef91cbde10cd48b
Shin-E2 <sinhyejee322@gmail.com>
2025-04-28 14:27:45 +0900

    [Refactor/main] props를 통한 데이터 전달 방식으로 리팩토링 및 타입 정의 추가

--- src/components/home/(dashboard)/diary/list/diary-section/index.tsx
+++ src/components/home/(dashboard)/diary/list/diary-section/index.tsx
@@ -1,10 +1,18 @@
 "use client";
 
-import useDiaryList from "@/app/(dashboard)/diary/hook";
 import { DiaryListDiaryCard } from "../diary-card";
+import { Diary } from "@/app/(dashboard)/diary/types";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-export default function DiaryListDiarySection() {
-  const { diaries, router } = useDiaryList();
+interface DiaryListDiarySectionProps {
+  diaries: Diary[];
+  router: AppRouterInstance;
+}
+
+export default function DiaryListDiarySection({
+  diaries,
+  router,
+}: DiaryListDiarySectionProps) {
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 mb-6">
       {diaries.map((diary) => (

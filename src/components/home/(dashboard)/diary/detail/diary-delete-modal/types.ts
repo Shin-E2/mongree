1dbf90028c866ece2d20c10c63ad2ad854869c7c
Shin-E2 <sinhyejee322@gmail.com>
2026-04-28 12:15:41 +0900

    [Fix/main] 일기 삭제 모달 표시와 상태 개선

--- src/components/home/(dashboard)/diary/detail/diary-delete-modal/types.ts
+++ src/components/home/(dashboard)/diary/detail/diary-delete-modal/types.ts
@@ -1,9 +1,7 @@
-import { ModalStandardFitFit } from "@/commons/components/modal";
-
 export interface DiaryDeleteModalProps {
   isOpen: boolean;
   onClose: () => void;
   deleteError: string;
   isPending: boolean;
   handleDeleteWithError: () => Promise<void>;
-} 
+}

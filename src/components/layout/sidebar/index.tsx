b5c6f4b142b873aa4ef77cb4512f9288ba95b485
Shin-E2 <sinhyejee322@gmail.com>
2026-04-29 20:37:33 +0900

    [Feat/app-shell] Sidebar 3rem 슬림 구조로 교체, 브랜드블록·프로필섹션 제거

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -1,22 +1,12 @@
 import styles from "./styles.module.css";
 import SidebarNavigation from "./navigation";
-import SidebarUserProfileSection from "./user-profile-section";
-import { ButtonTextLogo } from "@/commons/components/button-text";
-import { URL } from "@/commons/constants/global-url";
 import MobileNavigation from "./navigation/mobile-navigation";
 
 export default function SideBar() {
   return (
     <>
       <aside className={styles.aside}>
-        <div className={styles.aside_div}>
-          <div className={styles.brandBlock}>
-            <ButtonTextLogo href={URL().HOME} title="mongree" />
-            <p className={styles.brandCaption}>오늘의 감정을 하늘에 적어요</p>
-          </div>
-          <SidebarNavigation />
-          <SidebarUserProfileSection />
-        </div>
+        <SidebarNavigation />
       </aside>
 
       <nav className={styles.mobile_nav}>

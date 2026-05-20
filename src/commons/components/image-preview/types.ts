3fb38bbe4c5f234d7c7bd50d76b028ab23ae3af2
Shin-E2 <70641763+Shin-E2@users.noreply.github.com>
2026-05-21 08:27:13 +0900

    [Refactor/image-preview] 프로필 전용 타입에서 다중 이미지 옵션 제거

--- src/commons/components/image-preview/types.ts
+++ src/commons/components/image-preview/types.ts
@@ -3,8 +3,6 @@ import type { FieldValues, UseFormRegister } from "react-hook-form";
 export interface IImagePreviewBaseProps<T extends FieldValues> {
   cssprop: string;
   register?: UseFormRegister<T>;
-  multiple?: boolean;
-  maxImages?: number;
   className?: string;
 }
 
